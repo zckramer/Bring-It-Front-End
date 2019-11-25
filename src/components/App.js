ee8c2bc1bb3821fa2a1e940409d15ad5fd7e0a80
Richard Wilson <rcwilson88@gmail.com>
2019-11-25 15:17:26 -0500

    Created NavBar component

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,16 @@ const Http = require("../utils/Http");
 const UserCard = require('../components/UserCard')
 const Users = require('../components/Users')
 const HomePage = require('../components/HomePage')
+const NavBar = require('../components/NavBar')
 
 
 async function App (isLoggedIn) {
 
+
         // DEV -- EDITTING HOME PAGE
-        return Deact.render( await HomePage(), document.querySelector(".home-page-container"))    
+        return Deact.render(await NavBar(), document.querySelector(".home-page-container")   
+                
+        )
 
         // USER LOGIN    
         // return Deact.create("div", {class:"users-area"}, await Users.renderUsers())    
