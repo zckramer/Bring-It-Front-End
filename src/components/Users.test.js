ac6e7aef27bacc28c78a392b7cc896c3d0ce2a65
Zack Kramer <zckramer83@gmail.com>
2019-11-26 13:45:38 -0500

    began testing user controller

---
+++ src/components/Users.test.js
@@ -0,0 +1,12 @@
+const underTest = require('./Users')
+
+describe('renderUsers', () => {
+    test('should create an element for each user in the DB', async () => {
+        
+
+        const returnedUserCards = await underTest.renderUsers();
+        expect(returnedUserCards).toBeDefined
+        console.log(returnedUserCards)
+        // expect()
+    })
+})
