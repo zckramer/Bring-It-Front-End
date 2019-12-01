e9c2e4d1bdde6278c2bd00e0063af403cd94d7c2
Zachary Kramer <zckramer83@gmail.com>
2019-12-01 13:53:48 -0500

    hard coded items list

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -16,15 +16,22 @@ async function RenderEventForm () {
                 Deact.create("input", {class:"input-desc", placeholder:"Description"}, ""),
                 Deact.create("br", {}, ""),
                 Deact.create("section", {class:"input-friends"}, await Users.renderFriendsCheckbox()),
-                Deact.create("p", {class:"input-items"}, [
+                Deact.create("div", {class:"input-items"}, [
                     // checkboxes of items to bring
                     "checkboxes of items to bring",
-                    Deact.create("input", {type:"checkbox", class:"item-checkbox"}, "item #1")
+                    
+                    Deact.create("span", {class:"item-label"}, ["La Croix", Deact.create("input", {type:"checkbox", class:"item-checkbox", value: "5de3f5284a747d08689a7538"}, "")]),
+                    Deact.create("span", {class:"item-label"}, ["AA Batteries", Deact.create("input", {type:"checkbox", class:"item-checkbox", value: "5de3f5984a747d08689a753a"}, "")]),
+                    Deact.create("span", {class:"item-label"}, ["Disinfecting Wipes", Deact.create("input", {type:"checkbox", class:"item-checkbox", value: "5de3f5eb4a747d08689a753b"}, "")]),
+                    Deact.create("span", {class:"item-label"}, ["All Purpose Spray Adhesive", Deact.create("input", {type:"checkbox", class:"item-checkbox", value: "5de3f61d4a747d08689a753c"}, "")]),
+                    Deact.create("span", {class:"item-label"}, ["Toilet Paper", Deact.create("input", {type:"checkbox", class:"item-checkbox", value: "5de3f6954a747d08689a753d"}, "")]),
+                    Deact.create("span", {class:"item-label"}, ["Folding Chairs, Metal", Deact.create("input", {type:"checkbox", class:"item-checkbox", value: "5de3f6e64a747d08689a753e"}, "")]),
+                    Deact.create("span", {class:"item-label"}, ["Paper Table Cloths 8' x 8'", Deact.create("input", {type:"checkbox", class:"item-checkbox", value: "5de3f7174a747d08689a753f"}, "")]),
+                    Deact.create("span", {class:"item-label"}, ["Peanuts, Roasted, Salted", Deact.create("input", {type:"checkbox", class:"item-checkbox", value: "5de3f73c4a747d08689a7540"}, "")])
                 ]),
+
                 Deact.create ("button", {class:"event-submit"}, "Submit")
                 
-                
-
             ])
         
     ])
@@ -32,6 +39,12 @@ async function RenderEventForm () {
 }
 
 module.exports = RenderEventForm;
+
+{
+
+}
+
+
 
 /* <article class="event-form__container">
         <h2>Event Form</h2>
